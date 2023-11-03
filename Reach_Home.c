2807850492069ece48f5a9e06e5bd34944df3f9a
Rial hasan <111173502+Realhasa@users.noreply.github.com>
2023-11-03 22:32:31 +0530

    #include<stdio.h> int main() {
        int x,y,distance;
        
        
        scanf("%d%d",&x,&y);
        distance=5*x;
        
        
        if(distance>=y){
            printf("YES");
        }
        else{
            printf("NO");
        }
        
    }

---
+++ Reach_Home.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    
+    
+    int x,y,distance;
+    
+    
+    scanf("%d%d",&x,&y);
+    distance=5*x;
+    
+    
+    if(distance>=y){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    
+}
