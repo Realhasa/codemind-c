656013a3a9e36e956b6b82ac2dc58a3219ea6f38
Rial hasan <111173502+Realhasa@users.noreply.github.com>
2024-01-02 21:27:23 +0530

    #include<stdio.h> int main() {     char s[333];     scanf("%[^ ]s",s);     int count=0;     for(int i=0;s[i]!=NULL;i++){         switch (s[i]){              case 'a':             case 'e':             case 'i':             case 'o':             case 'u':             case 'A':             case 'E':             case 'I':             case 'O':             case 'U':                 count++;         }     }     printf("%d",count); }

--- Vowels_Count.c
+++ Vowels_Count.c
@@ -6,9 +6,19 @@ int main()
 ]s",s);
     int count=0;
     for(int i=0;s[i]!=NULL;i++){
-        if(s[i]==97 || s[i]==65 || s[i]==101 || s[i]==69|| s[i]==105|| s[i]==73 || s[i]==111 || s[i]==79 || s[i]==117 ||s[i]==85){
-            count=count+1;
+        switch (s[i]){
+             case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                count++;
         }
-    }    
+    }
     printf("%d",count);
 }
